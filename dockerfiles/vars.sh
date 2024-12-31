bc4df2481a710e38c8b1eccf24b4bc95575d978e
Nilton <niltonlqjr@gmail.com>
2024-12-30 23:10:11 -0300

    username in vars.sh!

--- dockerfiles/vars.sh
+++ dockerfiles/vars.sh
@@ -8,6 +8,8 @@ IMAGE_VERSION="22.04"                           # version of image
 CONTAINER_IMAGE=${IMAGE_NAME}:${IMAGE_VERSION}  # Name of image to be generated
 CONTAINER_NAME="LIBVIPS"
 
+CONTAINER_USER="experiments"			# username in container 
+
 HOST_UID=$(id -u) # get user ID from system to use in docker 
 HOST_GID=$(id -g) # get user ID from system to use in docker
 
